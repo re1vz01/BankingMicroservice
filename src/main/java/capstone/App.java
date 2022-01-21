bd119c6de052057c89d0f5d9998c4b22faa19f17
Reivin <reivin@gmail.com>
2022-01-21 09:39:37 +0000

    Edited by dev1

--- src/main/java/capstone/App.java
+++ src/main/java/capstone/App.java
@@ -9,5 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+    	System.out.println("Edited by Dev1");
+	System.out.println("Edited by Dev2");
     }
 }
